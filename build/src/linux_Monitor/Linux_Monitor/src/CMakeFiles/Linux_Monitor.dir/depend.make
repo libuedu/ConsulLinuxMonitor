72d58de052a987b6d5edf1987acbbbfbb19d7290
Leeve <libu316@nuaa.edu.cn>
2024-09-03 16:19:17 +0800

    [添加.gitignore文件] master分支修改

--- build/src/linux_Monitor/Linux_Monitor/src/CMakeFiles/Linux_Monitor.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Linux_Monitor.
-# This may be replaced when dependencies are built.
--- build/src/linux_Monitor/display_monitor/CMakeFiles/display_test.dir/display_test_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/linux_Monitor/display_monitor/CMakeFiles/display_test.dir/display_test_autogen/mocs_compilation.cpp.o: \
- /work/build/src/linux_Monitor/display_monitor/display_test_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
